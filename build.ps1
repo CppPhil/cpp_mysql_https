4c352d3577affd798287a9b1a31dea875f0da2f9
Phillip Schackier <CppPhil@users.noreply.github.com>
2022-07-30 18:00:43 +0200

    Update build.ps1 to build both Debug and Release

--- build.ps1
+++ build.ps1
@@ -7,6 +7,15 @@ if (-Not (Test-Path -Path $build_dir)) {
 
 Set-Location $build_dir
 $cpu_count = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . --config Debug --parallel $cpu_count
+
+if (-Not ($LASTEXITCODE -eq "0")) {
+  Write-Output "cmake --build for Debug mode failed!"
+  Pop-Location
+  exit 1
+}
+
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release --parallel $cpu_count
 
