bec88a26bcbce8b7ff5f72cfadb14478e9c20b33
Phillip Schackier <CppPhil@users.noreply.github.com>
2022-07-31 13:06:48 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
+#include <cstdlib>
 
+#include <iostream>
 #include <string>
 
 #include <Poco/Data/MySQL/Connector.h>
@@ -15,8 +17,35 @@ int main()
 {
   Poco::Data::MySQL::Connector::registerConnector();
   const std::string connectionString{
-    "host=127.0.0.1;user=root;password=letmein;compress=true;auto-reconnect="
+    "host=127.0.0.1;user=root;password=letmein;db=cpp_db;compress=true;auto-"
+    "reconnect="
     "true"};
-  Poco::Data::Session test{Poco::Data::SessionFactory::instance().create(
+  Poco::Data::Session session{Poco::Data::SessionFactory::instance().create(
     Poco::Data::MySQL::Connector::KEY, connectionString)};
+
+  if (!session.isConnected()) {
+    std::cerr << "session is not connected!\n";
+    return EXIT_FAILURE;
+  }
+
+  if (!session.isGood()) {
+    std::cerr << "session is bad!\n";
+    return EXIT_FAILURE;
+  }
+
+  Customer              customer{1, "Peter Smith"};
+  Poco::Data::Statement insertStatement{session};
+  using namespace Poco::Data::Keywords;
+  insertStatement << "INSERT INTO customer VALUES(?)", use(customer.name);
+  const std::size_t rowsAffected{insertStatement.execute()};
+  std::cout << "rowsAffected: " << rowsAffected << '\n';
+
+  if (!insertStatement.done()) {
+    std::cerr << "stament isn't done.\n";
+    return EXIT_FAILURE;
+  }
+
+  session.commit();
+  session.close();
+  return EXIT_SUCCESS;
 }
